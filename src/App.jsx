946436b025b23cd05c8179d3c68f4f979e749f90
mattgauu <mateogaunatesta@gmail.com>
2024-12-08 19:57:05 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailCont
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartProvider } from './context/CartContext'
 import Cart from './components/Cart/Cart'
-import Checkout from './components/checkout/checkout'
+import Checkout from './components/Checkout/Checkout'
 
 
 
